1926141eb181f3c201819335dae3c90f2123fa7a
Gio2025 <george.athanasopoulos46@student.qcc.cuny.edu>
2025-11-06 15:45:58 -0500

    Complete Lab 12

---
+++ lab12_props/src/comments.js
@@ -0,0 +1,24 @@
+import React from "react"; 
+import avataruser1 from './images/avatar_batman.png'
+import './App.css'
+
+const User = function(props){
+    return(
+        <>
+            <section className="card">
+                <a><img src={props.image} ></img></a>
+                <div>
+                    <a className="author">{props.name}</a>
+                </div>
+                <div className="metadata">
+                    <p>Posted on <span className="date">{props.date}</span></p>
+                </div>
+                <div className="comment">
+                    <p><i>"{props.msg}"</i></p>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default User
