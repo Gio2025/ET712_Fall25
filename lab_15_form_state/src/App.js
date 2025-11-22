fc5e486b6e0750f620f0674ce5285e0fa67beb5a
Gio2025 <george.athanasopoulos46@student.qcc.cuny.edu>
2025-11-21 22:42:06 -0500

    Completed Lab 15

--- lab_15_form_state/src/App.js
+++ lab_15_form_state/src/App.js
@@ -3,25 +3,19 @@ import React, { useState } from 'react';
 
 function App() {
 
-  // State for all text, number, and select inputs
   const [inputs, setInputs] = useState({});
 
-  // Function to handle ALL input changes (including gender)
   const handle_changes = (event) => {
     const name = event.target.name;
     const value = event.target.value;
-
     setInputs(values => ({ ...values, [name]: value }));
   };
 
-  // Set state for comments
-  const [textcomment, setComment] = useState("")
-  // Function to collect the comments
+  const [textcomment, setComment] = useState("");
   const submitted_comments = function(event){
-    setComment(event.target.value)
+    setComment(event.target.value);
   }
 
-  // Function to submit form
   const submitform = (event) => {
     event.preventDefault();
     console.log("Form submitted:", inputs);
@@ -46,6 +40,20 @@ function App() {
             />
           </div>
 
+          {/* Password input */}
+          <div className='formgroup'>
+            <label htmlFor='password'>Enter a password:</label>
+            <input
+              type='password'
+              id='password'
+              className='inputstyle'
+              name='password'
+              placeholder='Type your password...'
+              value={inputs.password || ""}
+              onChange={handle_changes}
+            />
+          </div>
+
           <div className='formgroup'>
             <label htmlFor='age'>Enter your age:</label>
             <input
@@ -59,44 +67,40 @@ function App() {
             />
           </div>
 
-          {/** Select a gender */}
           <div className='formgroup'>
-            <label> Select a Gender:
+            <label htmlFor='gender'>Select a Gender:</label>
             <select
-            name="gender"
-            value={inputs.gender || ""}
-            onChange={handle_changes}
-          >
-            <option value="">Select gender...</option>
-            <option value="female">Female</option>
-            <option value="male">Male</option>
-            <option value="other">Other</option>
-          </select>
-          </label>
+              id='gender'
+              name="gender"
+              value={inputs.gender || ""}
+              onChange={handle_changes}
+            >
+              <option value="">Select gender...</option>
+              <option value="female">Female</option>
+              <option value="male">Male</option>
+              <option value="other">Other</option>
+            </select>
           </div>
 
-          {/** Comments using textarea */}
           <div className='formgroup'>
-            <label for="comments">Any Suggestions</label>
+            <label htmlFor="comments">Any Suggestions</label>
             <textarea 
-            id='comments'
-            value={textcomment}
-            onChange={submitted_comments}
-            placeholder='Type a comment'    
+              id='comments'
+              value={textcomment}
+              onChange={submitted_comments}
+              placeholder='Type a comment'
             />
           </div>
-          
 
-          {/** Submit Form */}
           <input type='submit' className='btnsubmit' />
         </fieldset>
       </form>
 
-      {/** Show collected values */}
       <p className='printusername'>Username = {inputs.username}</p>
       <p className='printusername'>Age = {inputs.age}</p>
       <p className='printusername'>Gender = {inputs.gender}</p>
       <p className='printusername'>Comments = {textcomment}</p>
+      <p className='printusername'>Password = {inputs.password}</p>
 
     </div>
   );
